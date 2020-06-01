351ffa840888a10628c4e3d630014425e89fb7c5
Surfsol <surfsol@protonamil.com>
2020-06-01 18:09:09 -0300

    homework

--- src/04_printing.py
+++ src/04_printing.py
@@ -11,7 +11,11 @@ z = "I like turtles!"
 # Using the printf operator (%), print the following feeding in the values of x,
 # y, and z:
 # x is 10, y is 2.25, z is "I like turtles!"
-
+x = 10
+y = 2.25
+z = "I like turtles!"
+print("I am %d, I have %f dollars and %s" %(x, y, z))
 # Use the 'format' string method to print the same thing
-
+print("I am {}, I have {} dollars and {}".format(10, 2.25, "I like turtles!") )
 # Finally, print the same thing using an f-string
+print("I am {0}, I have {1} dollars and {said}".format(10, 2.25, said= "I like turtles!") )
